75926e801bda8649424ae23be5ce189a6a0bf3b6
teejay001 <judeadekola357@gmail.com>
2025-06-11 03:56:08 -0700

    Initial commit of Flask Postgres app

---
+++ db/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS greetings (
+    id SERIAL PRIMARY KEY,
+    message TEXT NOT NULL
+);
+
+INSERT INTO greetings (message) VALUES ('Hello from Xapic Devops Group1 Flask + Postgres webapp😊😁😉 !');
